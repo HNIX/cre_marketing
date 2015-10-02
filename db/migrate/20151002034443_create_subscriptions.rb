3f64848e576eecbdfb64f902735162848f6ddb43
Prelang Builder <builder@prelang.com>
2015-10-02 03:44:44 +0000

    Generated model Subscription with migration

---
+++ db/migrate/20151002034443_create_subscriptions.rb
@@ -0,0 +1,12 @@
+class CreateSubscriptions < ActiveRecord::Migration
+  def change
+    create_table :subscriptions do |t|
+      t.string :email
+      t.string :name
+      t.boolean :subscribed
+      t.boolean :confirmed
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/subscriptions.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :subscription do
+    email "MyString"
+name "MyString"
+subscribed false
+confirmed false
+  end
+
+end
